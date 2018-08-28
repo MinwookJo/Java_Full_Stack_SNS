051e821e346ad58c433f839eba321b250966467d
MinwookJo <alsdnr0537@naver.com>
2018-08-28 19:53:16 +0900

    .
    .

---
+++ mini_sns/src/main/java/kr/hs/dgsw/mini_sns/MiniSnsApplication.java
@@ -0,0 +1,12 @@
+package kr.hs.dgsw.mini_sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniSnsApplication.class, args);
+	}
+}
